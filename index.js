b5239df5cc54edcd91c29b7112ab070f7459f863
Chris Cardle <cdcardle@gmail.com>
2018-09-28 15:02:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup_location) {
+  let distance;
+  if (pickup_location >= 42) {
+    distance = pickup_location - 42;
+  } else if (pickup_location <= 41) {
+    distance = 42 - pickup_location;
+  } else {
+    return 'Please enter a number!'
+  }
+  return distance;
+}
